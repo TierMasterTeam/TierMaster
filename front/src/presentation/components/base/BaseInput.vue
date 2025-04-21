9be7f3b42ef97816d1dc2917788a7055a36516c3
Arthur FRIN <frin.arthur@gmail.com>
2025-04-21 22:16:04 +0200

    feat(front): enhance TemplateCreationView with category addition and update Button component defaults feat(front): add BaseInput component for user input handling feat(api): create get tierlist and ping API client configurations style(front): update CSS variables for fonts

---
+++ front/src/presentation/components/base/BaseInput.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const model = defineModel<string>()
+</script>
+
+<template>
+  <input
+    v-bind="$attrs"
+    v-model="model"
+    class="rounded-lg px-4 py-2 bg-white-custom text-dark-gray-custom outline-none text-base"
+  />
+</template>
--- front/src/presentation/views/TemplateCreationView.vue
+++ front/src/presentation/views/TemplateCreationView.vue
@@ -1,12 +1,59 @@
 <script setup lang="ts">
 import Button from '../components/base/Button.vue';
+import BaseInput from '../components/base/BaseInput.vue';
+import { ref } from 'vue';
+const templateName = ref<string>('');
+const templateCategories = ref<string[]>([]);
+const currentCategory = ref<string>('');
+
+const addCategory = () => {
+  if (currentCategory.value.trim() !== '') {
+    templateCategories.value.push(currentCategory.value.trim());
+    currentCategory.value = '';
+  }
+};
 
 
 </script>
 
 <template>
-  <main>
-    <Button variant="primary" type="button" size="md" icon="plus" class="mb-4">
+  <main class="m-16">
+    <label for="templateName">
+      <h1 class="text-[40px] font-jersey">Tierlist Title</h1>
+    </label>
+    <BaseInput
+      id="templateName"
+      type="text"
+      placeholder="My Tierlist title ..."
+      class="mb-4 w-1/3 max-w-2xl"
+      v-model="templateName"
+    />
+    <label for="category">
+      <h2 class="text-2xl font-jersey">Add #categories</h2>
+    </label>
+    <BaseInput
+      id="category"
+      type="text"
+      placeholder="category ..."
+      class="mb-4 max-w-sm"
+      v-model="currentCategory"
+    />
+    <Button
+      variant="primary"
+      type="button"
+      size="md"
+      icon="plus"
+      class="ml-4 inline-flex"
+      @click="addCategory"
+    >
+      Add Category
+  </Button>
+    <ul class="flex gap-2">
+      <li v-for="(category, index) in templateCategories" :key="index">
+        <span class="text-xl font-jersey bg-gray-custom rounded-full px-2 py-2">{{ category }}</span>
+      </li>
+    </ul>
+    <Button variant="primary" type="button" size="md" icon="plus" class="mt-4">
       Create Template
     </Button>
   </main>
