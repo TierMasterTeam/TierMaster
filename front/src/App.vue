7de391c65b538ab4f884d8fb3f7cde7248493ff8
Arthur FRIN <frin.arthur@gmail.com>
2025-04-25 15:25:51 +0200

    feat(front): implement RGPD banner component for cookie consent

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import RgpdBanner from './presentation/components/RgpdBanner.vue'
 </script>
 
 <template>
+  <RgpdBanner />
   <RouterView />
 </template>
