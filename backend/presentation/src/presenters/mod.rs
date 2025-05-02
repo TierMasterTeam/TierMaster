c7272297c09fa9a511c1c1cb34c434aa4849b367
AdrienDhmx <105926516+AdrienDhmx@users.noreply.github.com>
2025-05-02 18:41:13 +0200

    feat(api): return the user on login and signup

--- backend/presentation/src/presenters/mod.rs
+++ backend/presentation/src/presenters/mod.rs
@@ -4,6 +4,7 @@ mod card_presenter;
 mod grade_presenter;
 mod update_tierlist_presenter;
 mod credentials_presenter;
+mod user_presenter;
 
 pub use create_tierlist_presenter::CreateTierlistPresenter;
 pub use tierlist_presenter::TierlistPresenter;
@@ -11,5 +12,7 @@ pub use update_tierlist_presenter::UpdateTierlistPresenter;
 
 pub use card_presenter::CardPresenter;
 pub use grade_presenter::GradePresenter;
+
+pub use user_presenter::UserPresenter;
 
 pub use credentials_presenter::CredentialsPresenter;
