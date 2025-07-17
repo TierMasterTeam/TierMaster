697088c2a9e734b9b1d771793003f6c443c4d1df
Arthur FRIN <frin.arthur@gmail.com>
2025-07-17 18:34:32 +0200

    feat: update contact information and enhance home page descriptions in English and French locales

--- front/src/infra/locales/en/footer.ts
+++ front/src/infra/locales/en/footer.ts
@@ -2,6 +2,6 @@ export default {
   privacy: 'Privacy Policy',
   terms: 'Terms of Use',
   noData: 'No personal data collected • No use of cookies',
-  contact: 'contact: lorem ipsum',
+  contact: 'contact: team.tiermaster@gmail.com',
   copyright: '© 2025 TierMaster — All rights reserved',
 }
--- front/src/infra/locales/en/home.ts
+++ front/src/infra/locales/en/home.ts
@@ -6,7 +6,7 @@ export default {
   popularTemplates: 'Popular Templates',
   whatsNew: "What's New",
   whatsNewDesc:
-    'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
+    "Discover tierlist templates created by the community, play in real time with your friends, and create your own custom tierlists. Share, compare, and have fun together on TierMaster!",
   recentTemplates: 'Recent Templates',
   results: 'results',
   noTemplatesFound: 'No templates found matching "{query}"',
